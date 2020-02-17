9b4aad1056ab07d7997628aae9788d5232ef43dc
Valentin <blxyon@yahoo.com>
2020-02-17 16:08:34 +0000

    begining

---
+++ src/mainMenu/Schedule.java
@@ -0,0 +1,155 @@
+package mainMenu;
+
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class Schedule {
+    //shit class cos lack of experience
+    private SimpleIntegerProperty position;
+    private SimpleIntegerProperty ride;
+    private SimpleStringProperty st1;//9:00
+    private SimpleStringProperty st2;//10:00...
+    private SimpleStringProperty st3;
+    private SimpleStringProperty st4;
+    private SimpleStringProperty st5;
+    private SimpleStringProperty st6;
+    private SimpleStringProperty st7;
+    private SimpleStringProperty st8;
+
+        public Schedule(Integer position, Integer ride, String st1, String st2,
+                        String st3, String st4, String st5, String st6,
+                        String st7, String st8) {
+        this.position = new SimpleIntegerProperty(position);
+        this.ride = new SimpleIntegerProperty(ride);
+        this.st1=new SimpleStringProperty(st1);
+        this.st2=new SimpleStringProperty(st2);
+        this.st3=new SimpleStringProperty(st3);
+        this.st4=new SimpleStringProperty(st4);
+        this.st5=new SimpleStringProperty(st5);
+        this.st6=new SimpleStringProperty(st6);
+        this.st7=new SimpleStringProperty(st7);
+        this.st8=new SimpleStringProperty(st8);
+
+
+    }
+
+    public int getPosition() {
+        return position.get();
+    }
+
+    public SimpleIntegerProperty positionProperty() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position.set(position);
+    }
+
+    public int getRide() {
+        return ride.get();
+    }
+
+    public SimpleIntegerProperty rideProperty() {
+        return ride;
+    }
+
+    public void setRide(int ride) {
+        this.ride.set(ride);
+    }
+
+    public String getSt1() {
+        return st1.get();
+    }
+
+    public SimpleStringProperty st1Property() {
+        return st1;
+    }
+
+    public void setSt1(String st1) {
+        this.st1.set(st1);
+    }
+
+    public String getSt2() {
+        return st2.get();
+    }
+
+    public SimpleStringProperty st2Property() {
+        return st2;
+    }
+
+    public void setSt2(String st2) {
+        this.st2.set(st2);
+    }
+
+    public String getSt3() {
+        return st3.get();
+    }
+
+    public SimpleStringProperty st3Property() {
+        return st3;
+    }
+
+    public void setSt3(String st3) {
+        this.st3.set(st3);
+    }
+
+    public String getSt4() {
+        return st4.get();
+    }
+
+    public SimpleStringProperty st4Property() {
+        return st4;
+    }
+
+    public void setSt4(String st4) {
+        this.st4.set(st4);
+    }
+
+    public String getSt5() {
+        return st5.get();
+    }
+
+    public SimpleStringProperty st5Property() {
+        return st5;
+    }
+
+    public void setSt5(String st5) {
+        this.st5.set(st5);
+    }
+
+    public String getSt6() {
+        return st6.get();
+    }
+
+    public SimpleStringProperty st6Property() {
+        return st6;
+    }
+
+    public void setSt6(String st6) {
+        this.st6.set(st6);
+    }
+
+    public String getSt7() {
+        return st7.get();
+    }
+
+    public SimpleStringProperty st7Property() {
+        return st7;
+    }
+
+    public void setSt7(String st7) {
+        this.st7.set(st7);
+    }
+
+    public String getSt8() {
+        return st8.get();
+    }
+
+    public SimpleStringProperty st8Property() {
+        return st8;
+    }
+
+    public void setSt8(String st8) {
+        this.st8.set(st8);
+    }
+}
---
+++ src/mainMenu/dbCreator.java
@@ -0,0 +1,97 @@
+package mainMenu;
+
+import java.io.File;
+import java.sql.*;
+
+
+public class dbCreator {
+    String fileName;
+    Connection conn;
+
+    public dbCreator(String fileName) {this.fileName=fileName+".db";
+    }
+
+
+
+    public int createNewDatabase() {
+
+        int createdDB=2;
+
+        if(!(new File(fileName)).exists())
+        {
+            String url = "jdbc:sqlite:" + fileName;
+
+            try{
+                conn = DriverManager.getConnection(url);
+                if (conn != null) {
+                    DatabaseMetaData meta = conn.getMetaData();
+                    System.out.println("The driver name is " + meta.getDriverName());
+                    System.out.println("A new database has been created.");
+
+                    //connect();
+
+                    String sql=getSQLTableCreation();
+
+                    Statement stmt = conn.createStatement();
+                    stmt.execute(sql);
+
+                    createdDB=0;
+                }
+
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }else {
+            System.out.println("DB file exists already!");
+            createdDB=1;}
+        return createdDB;
+    }
+
+    private String getSQLTableCreation() {
+        String sql="CREATE TABLE IF NOT EXISTS " +
+                "CREATE TABLE IF NOT EXISTS ppl (\n"
+                + "	id integer PRIMARY KEY autoincrement,\n"
+                + "	fName text NOT NULL,\n"
+                + "	email text NOT NULL,\n"
+                + "	type text NOT NULL\n"
+                + ");";
+        return sql;
+    }
+
+    public void connect() {
+        try {
+            // db parameters
+            String url = "jdbc:sqlite:"+fileName;
+            // create a connection to the database
+            conn = DriverManager.getConnection(url);
+
+            System.out.println("Connection to SQLite has been established.");
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+
+    /*public void insert(String fName ,String sName, String email, String type) {
+        String sql = "INSERT INTO 'TABLENAME''(FIELDS) VALUES(?,?,?,?)";
+
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, value for first entry);
+            pstmt.setString(2, value second entry);
+            pstmt.setString(3, value third entry);
+            pstmt.setString(4, value fourth entry);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }*/
+
+}
