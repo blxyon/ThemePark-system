9ac6ea3017c21ebaa3826c996f12207a578d4a8b
Valentin <blxyon@yahoo.com>
2020-02-20 15:22:28 +0000

    hannan s fxml added

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("mainMenu/menu#.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("mainMenu/menu.fxml"));
         primaryStage.setTitle("Main menu");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
