4f260965f436fb63a3f5cc40814666202fcceef3
iljaiskovs <iljaiskovs@users.noreply.github.com>
2020-03-01 19:43:36 +0000

    add swap parsing and storage

--- src/comms/RideTerminalBackend.java
+++ src/comms/RideTerminalBackend.java
@@ -4,15 +4,22 @@ import java.io.IOException;
 import java.net.Socket;
 import java.util.LinkedList;
 
+import mainMenu.swapManagament.Swap;
+
 public class RideTerminalBackend implements Runnable{
 	private String name;
 	private ThreadedSocket managerTerminal = null;
 	private boolean running = false;
 	private LinkedList<String> currentWorkers;
 	
+	private LinkedList<Swap> swaps;
+	private SwapParser swapParser;
+	
 	public RideTerminalBackend(String name) {
 		this.name = name;
 		currentWorkers = new LinkedList<String>();
+		swaps = new LinkedList<Swap>();
+		swapParser = new SwapParser();
 	}
 	
 	public void run() {
@@ -82,8 +89,13 @@ public class RideTerminalBackend implements Runnable{
 	
 	private void processMessage(String message) {
 		String[] elements = message.split(",");
-		if (elements[0] == "type") {
-			//do stuff
+		if (elements[0] == "swap") {
+			addSwap(message);
 		}
+	}
+	
+	private void addSwap(String swapString) {
+		Swap newSwap = swapParser.fromString(swapString);
+		swaps.add(newSwap);
 	}
 }
