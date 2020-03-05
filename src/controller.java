bd2d986b2a442c62346f4c14de8e505712f82ea7
Valentin <blxyon@yahoo.com>
2020-03-05 02:36:23 +0000

    vikings ui manipulation

--- src/controller.java
+++ src/controller.java
@@ -1,15 +1,25 @@
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import comms.RideTerminalBackend;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -17,22 +27,123 @@ import javafx.stage.Stage;
 public class controller implements Initializable {
     @FXML Button otherMessB;
     @FXML Text terminalNameL;
+
+
 //get all the buttons/tables from all windows here
     
     RideTerminalBackend backendInstance;
-    
+
+    @FXML private TableView<Staff> OperatorTable;
+    @FXML private TableColumn<Staff, String> OCategory;
+    @FXML private TableColumn<Staff, String> OName;
+    @FXML private TableColumn<Staff, String> OStartT;
+    @FXML private TableColumn<Staff, String> OEndT;
+    @FXML private TableView<Staff> AttendantPTable;
+    @FXML private TableColumn<Staff, String> APCategory;
+    @FXML private TableColumn<Staff, String> APName;
+    @FXML private TableColumn<Staff, String> APStartT;
+    @FXML private TableColumn<Staff, String> APEndT;
+    @FXML private TableView<Staff> AttendantSTable;
+    @FXML private TableColumn<Staff, String> ASCategory;
+    @FXML private TableColumn<Staff, String> ASName;
+    @FXML private TableColumn<Staff, String> ASStartT;
+    @FXML private TableColumn<Staff, String> ASEndT;
+    @FXML private TableView<Staff> ServiceTable;
+    @FXML private TableColumn<Staff, String> SCategory;
+    @FXML private TableColumn<Staff, String> SName;
+    @FXML private TableColumn<Staff, String> SStartT;
+    @FXML private TableColumn<Staff, String> SEndT;
+
+
+    @FXML private Button signInB;
+    @FXML private Button signOutB;
+    @FXML private ListView staffLoggedList;
+
+
+    String[] staffLogInEMULATED = { "Hannan", "BOSS", "HUGO",
+            "WILL", "IlJa","ShuSh" };
+    Integer indexStaffArr=0;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        //initialization
+        //backendInstance = RideTerminalBackend.getInstance();
+        System.out.println("initializing");
+
+        OCategory.setCellValueFactory(new PropertyValueFactory<Staff, String>("Category"));
+        OName.setCellValueFactory(new PropertyValueFactory<Staff, String>("Name"));
+        OStartT.setCellValueFactory(new PropertyValueFactory<Staff, String>("StartT"));
+        OEndT.setCellValueFactory(new PropertyValueFactory<Staff, String>("EndT"));
+
+        APCategory.setCellValueFactory(new PropertyValueFactory<Staff, String>("Category"));
+        APName.setCellValueFactory(new PropertyValueFactory<Staff, String>("Name"));
+        APStartT.setCellValueFactory(new PropertyValueFactory<Staff, String>("StartT"));
+        APEndT.setCellValueFactory(new PropertyValueFactory<Staff, String>("EndT"));
+
+        ASCategory.setCellValueFactory(new PropertyValueFactory<Staff, String>("Category"));
+        ASName.setCellValueFactory(new PropertyValueFactory<Staff, String>("Name"));
+        ASStartT.setCellValueFactory(new PropertyValueFactory<Staff, String>("StartT"));
+        ASEndT.setCellValueFactory(new PropertyValueFactory<Staff, String>("EndT"));
+
+        SCategory.setCellValueFactory(new PropertyValueFactory<Staff, String>("Category"));
+        SName.setCellValueFactory(new PropertyValueFactory<Staff, String>("Name"));
+        SStartT.setCellValueFactory(new PropertyValueFactory<Staff, String>("StartT"));
+        SEndT.setCellValueFactory(new PropertyValueFactory<Staff, String>("EndT"));
+
+        ObservableList dataO=getInitialTableDataO();
+        ObservableList dataAP=getInitialTableDataAP();
+        ObservableList dataAS=getInitialTableDataAS();
+        ObservableList dataS=getInitialTableDataS();
+
+        OperatorTable.setItems(dataO);
+        AttendantPTable.setItems(dataAP);
+        AttendantSTable.setItems(dataAS);
+        ServiceTable.setItems(dataS);
+
+        OperatorTable.setEditable(true);
+        OCategory.setCellFactory(TextFieldTableCell.forTableColumn());
+        OName.setCellFactory(TextFieldTableCell.forTableColumn());
+        OStartT.setCellFactory(TextFieldTableCell.forTableColumn());
+        OEndT.setCellFactory(TextFieldTableCell.forTableColumn());
+
+        AttendantPTable.setEditable(true);
+        APCategory.setCellFactory(TextFieldTableCell.forTableColumn());
+        APName.setCellFactory(TextFieldTableCell.forTableColumn());
+        APStartT.setCellFactory(TextFieldTableCell.forTableColumn());
+        APEndT.setCellFactory(TextFieldTableCell.forTableColumn());
+
+        AttendantSTable.setEditable(true);
+        ASCategory.setCellFactory(TextFieldTableCell.forTableColumn());
+        ASName.setCellFactory(TextFieldTableCell.forTableColumn());
+        ASStartT.setCellFactory(TextFieldTableCell.forTableColumn());
+        ASEndT.setCellFactory(TextFieldTableCell.forTableColumn());
+
+        ServiceTable.setEditable(true);
+        SCategory.setCellFactory(TextFieldTableCell.forTableColumn());
+        SName.setCellFactory(TextFieldTableCell.forTableColumn());
+        SStartT.setCellFactory(TextFieldTableCell.forTableColumn());
+        SEndT.setCellFactory(TextFieldTableCell.forTableColumn());
+    }
+    public void signIn(ActionEvent actionEvent)
+    {
+        //time function perhaps(while ppl scan their fingerprint)
+        if(indexStaffArr==6) indexStaffArr=0;
+        staffLoggedList.getItems().add(staffLogInEMULATED[indexStaffArr]);
+        indexStaffArr++;
+    }
+    public void signOut(ActionEvent actionEvent) {
+        indexStaffArr=staffLoggedList.getSelectionModel().getSelectedIndex();
+        staffLoggedList.getItems().remove(staffLoggedList.getSelectionModel().getSelectedItem());
+
+    }
+
     public String getTerminalName()
-            {
-                try
-                {
-                    System.out.println(terminalNameL.getText());
-                    return terminalNameL.getText();
-                }catch (NullPointerException e) {
-                    e.printStackTrace();
-                    return "noName";
-
-        }
-
+    {
+        try
+        {
+            return terminalNameL.getText();
+        }catch (NullPointerException e) {
+            e.printStackTrace();
+            return "noName"; }
     }
     public void callBreak(){
     	backendInstance.sendAlert("break");
@@ -78,9 +189,68 @@ public class controller implements Initializable {
 
     }
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-    	backendInstance = RideTerminalBackend.getInstance();
-    	System.out.println("initializing");
+
+    public ObservableList getInitialTableDataO() {
+
+        List list = new ArrayList();
+        Staff p=new Staff("asf","hannan","9","10","Viking's River Splash");
+        Staff p2=new Staff("asf","Will","9","10","Ninjago");
+        Staff[] arr={p,p2};
+        for(int i=0;i<arr.length;i++) {
+            if ((arr[i].getRide()).equals(getTerminalName())) {
+                list.add(arr[i]);
+            }
+        }
+
+        ObservableList data = FXCollections.observableList(list);
+
+        return data;
+    }
+    public ObservableList getInitialTableDataAP() {
+
+        List list = new ArrayList();
+        Staff p=new Staff("asf","hannan2222","9","10","Viking's River Splash");
+        Staff p2=new Staff("asf","Will2222222","9","10","Ninjago");
+        Staff[] arr={p,p2};
+        for(int i=0;i<arr.length;i++) {
+            if ((arr[i].getRide()).equals(getTerminalName())) {
+                list.add(arr[i]);
+            }
+        }
+        ObservableList data = FXCollections.observableList(list);
+
+        return data;
+    }
+    public ObservableList getInitialTableDataAS() {
+
+        List list = new ArrayList();
+        Staff p=new Staff("asf","hannan3333333","9","10","Viking's River Splash");
+        Staff p2=new Staff("asf","Will3333333333","9","10","Ninjago");
+        Staff[] arr={p,p2};
+        for(int i=0;i<arr.length;i++) {
+            if ((arr[i].getRide()).equals(getTerminalName())) {
+                list.add(arr[i]);
+            }
+        }
+
+        ObservableList data = FXCollections.observableList(list);
+
+        return data;
+    }
+    public ObservableList getInitialTableDataS() {
+
+        List list = new ArrayList();
+        Staff p=new Staff("asf","hannan33333344444","9","10","Viking's River Splash");
+        Staff p2=new Staff("asf","Will4444444","9","10","Ninjago");
+        Staff[] arr={p,p2};
+
+        for(int i=0;i<arr.length;i++) {
+            if ((arr[i].getRide()).equals(getTerminalName())) {
+                list.add(arr[i]);
+            }
+        }
+        ObservableList data = FXCollections.observableList(list);
+
+        return data;
     }
 }
