646b01ba7f56f764914be2a5ce6d08d206c0e80f
Valentin <blxyon@yahoo.com>
2020-03-05 00:52:31 +0000

    swap table functionalities

--- src/mainMenu/swapManagament/SwapC.java
+++ src/mainMenu/swapManagament/SwapC.java
@@ -2,11 +2,15 @@ package mainMenu.swapManagament;
 
 
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -20,6 +24,13 @@ public class SwapC implements Initializable {
     @FXML private TableColumn<Swap, String> T2;
     @FXML private TableColumn<Swap, String> T3;
     @FXML private TableColumn<Swap, String> T4;
+    @FXML private Button removeB;
+    @FXML private Button addB;
+    @FXML private TextField vF;
+    @FXML private TextField nF;
+    @FXML private TextField timeF;
+    @FXML private TextField dF;
+    @FXML private TextField sF;
 
 
     @Override
@@ -31,8 +42,28 @@ public class SwapC implements Initializable {
         T4.setCellValueFactory(new PropertyValueFactory<Swap, String>("T4"));
 
 
+
+
         modelSw=new SwapM();
         ObservableList data = modelSw.getInitialTableData();
         SwapTable.setItems(data);
+        SwapTable.setEditable(true);
+        time.setCellFactory(TextFieldTableCell.forTableColumn());
+        T1.setCellFactory(TextFieldTableCell.forTableColumn());
+        T2.setCellFactory(TextFieldTableCell.forTableColumn());
+        T3.setCellFactory(TextFieldTableCell.forTableColumn());
+        T4.setCellFactory(TextFieldTableCell.forTableColumn());
+    }
+
+    public void removeFromTable(ActionEvent actionEvent) {
+        ObservableList<Swap> allSwaps, selectedSwap;
+        allSwaps=SwapTable.getItems();
+        selectedSwap=SwapTable.getSelectionModel().getSelectedItems();
+        selectedSwap.forEach(allSwaps::remove);
+    }
+
+    public void addToTable(ActionEvent actionEvent) {
+        Swap newSwap=new Swap(timeF.getText(),vF.getText(),nF.getText(),sF.getText(),dF.getText());
+        SwapTable.getItems().add(newSwap);
     }
 }
